1f2a97cf655cad401790802630b4c3a475b4b590
Kyle Quilty <kquilty@gmail.com>
2025-11-05 10:58:22 -0500

    Fixed glaring errors shown in editor and on page: - "possible null" warning - missing <tr> - missing "key" attributes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
     const onChange = (e) => {
         const searchTerm = e.target.value;
 
-        document.getElementById("search-term").innerHTML = searchTerm;
+        document.getElementById("search-term")!.innerHTML = searchTerm;
 
         console.log("filtering advocates...");
         const filteredAdvocates = advocates.filter((advocate) => {
@@ -58,25 +58,27 @@ export default function Home() {
             <br />
             <table>
                 <thead>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>City</th>
-                    <th>Degree</th>
-                    <th>Specialties</th>
-                    <th>Years of Experience</th>
-                    <th>Phone Number</th>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>City</th>
+                        <th>Degree</th>
+                        <th>Specialties</th>
+                        <th>Years of Experience</th>
+                        <th>Phone Number</th>
+                    </tr>
                 </thead>
                 <tbody>
                     {filteredAdvocates.map((advocate) => {
                         return (
-                            <tr>
+                            <tr key={advocate.id}>
                                 <td>{advocate.firstName}</td>
                                 <td>{advocate.lastName}</td>
                                 <td>{advocate.city}</td>
                                 <td>{advocate.degree}</td>
                                 <td>
-                                    {advocate.specialties.map((s) => (
-                                        <div>{s}</div>
+                                    {advocate.specialties.map((s: string) => (
+                                        <div key={s}>{s}</div>
                                     ))}
                                 </td>
                                 <td>{advocate.yearsOfExperience}</td>
